65c833c9b5caf82b92531f8b10a5e04eb4774bcd
CrisDev3 <xactrismt1004@gmail.com>
2024-02-21 23:55:30 -0500

    included print

--- app/src/main/java/com/cristopherdev/gittutorial/MainActivity.kt
+++ app/src/main/java/com/cristopherdev/gittutorial/MainActivity.kt
@@ -12,22 +12,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.cristopherdev.gittutorial.ui.theme.GitTutorialTheme
 
+fun main (){
+    println("Esto es una activ")
+}
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GitTutorialTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
                 }
             }
         }
     }
 }
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
