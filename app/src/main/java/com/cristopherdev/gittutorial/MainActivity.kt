9fa635e0d12a0e0882926c92a0ea47c846e5ed98
CrisDev3 <xactrismt1004@gmail.com>
2024-02-22 21:06:36 -0500

    deleted prints

--- app/src/main/java/com/cristopherdev/gittutorial/MainActivity.kt
+++ app/src/main/java/com/cristopherdev/gittutorial/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.cristopherdev.gittutorial
 
 fun main (){
-    println("Nombre 1")
-    println("Apellido 1")
-    println("Nombre 2")
-    println("Apellido 2")
-    println("Nombre 3")
-    println("Apellido 3")
+
 
 }
